e11ccaec724bd9c48d1d9ff0c625d9e0a9338508
kirti arya <kirtiarya0504@gmail.com>
2026-02-26 22:24:18 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,38 @@
-from Bio.Seq import Seq
 from Bio import SeqIO
 import io
 
+# -------- READ FASTA --------
 def read_fasta(uploaded_file):
     stringio = io.StringIO(uploaded_file.getvalue().decode("utf-8"))
     record = SeqIO.read(stringio, "fasta")
     return str(record.seq)
 
-def gc_content(sequence):
-    g = sequence.count("G")
-    c = sequence.count("C")
-    return round((g + c) / len(sequence) * 100, 2)
+# -------- GC CONTENT --------
+def gc_content(seq):
+    g = seq.count("G")
+    c = seq.count("C")
+    return round((g + c) / len(seq) * 100, 2)
 
-def find_orfs(sequence):
-    seq = Seq(sequence)
+# -------- FIND ORFs --------
+def find_orfs(seq):
+    start = "ATG"
+    stop_codons = ["TAA", "TAG", "TGA"]
     orfs = []
-    for i in range(3):
-        trans = seq[i:].translate()
-        if "*" in str(trans):
-            orfs.append(str(trans))
 
+    for i in range(len(seq) - 3):
+        codon = seq[i:i+3]
+        if codon == start:
+            for j in range(i+3, len(seq)-3, 3):
+                stop = seq[j:j+3]
+                if stop in stop_codons:
+                    orfs.append(seq[i:j+3])
+                    break
     return orfs
+
+# -------- DETECT GENES --------
+def detect_genes(seq, db):
+    matches = []
+    for gene in db["gene"]:
+        if gene in seq:
+            matches.append(gene)
+    return matches
