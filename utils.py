2dd04c17f90d3ea778a2b43fd32f338fd4ea754d
kirti arya <kirtiarya0504@gmail.com>
2026-02-26 22:57:30 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,29 +1,21 @@
 from collections import Counter
 
-# ---------- FASTA READER ----------
+# ---------- FASTA ----------
 def read_fasta(uploaded_file):
-    if uploaded_file is None:
-        return None
-
     content = uploaded_file.getvalue().decode("utf-8")
-    lines = content.splitlines()
-
-    sequence = ""
-    for line in lines:
+    seq = ""
+    for line in content.splitlines():
         if not line.startswith(">"):
-            sequence += line.strip()
+            seq += line.strip()
+    return seq.upper()
 
-    return sequence.upper()
 
-
-# ---------- SEQUENCE STATS ----------
+# ---------- STATS ----------
 def sequence_stats(seq):
     length = len(seq)
     counts = Counter(seq)
-
     gc = counts.get("G", 0) + counts.get("C", 0)
     gc_content = round((gc / length) * 100, 2) if length else 0
-
     return length, gc_content, counts
 
 
@@ -43,11 +35,18 @@ def detect_genes(seq):
     return found
 
 
-# ---------- ORF DETECTION ----------
-def find_orfs(seq):
-    start = "ATG"
-    stops = ["TAA", "TAG", "TGA"]
+# ---------- BLAST-LIKE SEARCH ----------
+def similarity_search(seq):
+    results = {}
+    for gene, motif in ARG_DB.items():
+        score = seq.count(motif)
+        results[gene] = score
+    return results
 
+
+# ---------- ORF ----------
+def find_orfs(seq):
+    start, stops = "ATG", ["TAA", "TAG", "TGA"]
     orfs = []
     for i in range(len(seq)):
         if seq[i:i+3] == start:
@@ -58,7 +57,39 @@ def find_orfs(seq):
     return orfs
 
 
-# ---------- RESISTANCE SCORE ----------
-def resistance_score(gc, genes, orfs):
-    score = gc + (len(genes) * 20) + (len(orfs) * 5)
-    return round(score, 2)
+# ---------- PROTEIN TRANSLATION ----------
+CODON_TABLE = {
+    "ATA":"I","ATC":"I","ATT":"I","ATG":"M",
+    "ACA":"T","ACC":"T","ACG":"T","ACT":"T",
+    "AAC":"N","AAT":"N","AAA":"K","AAG":"K",
+    "AGC":"S","AGT":"S","AGA":"R","AGG":"R",
+    "CTA":"L","CTC":"L","CTG":"L","CTT":"L",
+    "CCA":"P","CCC":"P","CCG":"P","CCT":"P",
+    "CAC":"H","CAT":"H","CAA":"Q","CAG":"Q",
+    "CGA":"R","CGC":"R","CGG":"R","CGT":"R",
+    "GTA":"V","GTC":"V","GTG":"V","GTT":"V",
+    "GCA":"A","GCC":"A","GCG":"A","GCT":"A",
+    "GAC":"D","GAT":"D","GAA":"E","GAG":"E",
+    "GGA":"G","GGC":"G","GGG":"G","GGT":"G",
+    "TCA":"S","TCC":"S","TCG":"S","TCT":"S",
+    "TTC":"F","TTT":"F","TTA":"L","TTG":"L",
+    "TAC":"Y","TAT":"Y","TAA":"_","TAG":"_",
+    "TGC":"C","TGT":"C","TGA":"_","TGG":"W",
+}
+
+def translate(seq):
+    protein = ""
+    for i in range(0, len(seq)-2, 3):
+        codon = seq[i:i+3]
+        protein += CODON_TABLE.get(codon, "X")
+    return protein
+
+
+# ---------- MUTATION DETECTION ----------
+def mutation_scan(seq):
+    mutations = []
+    for gene, motif in ARG_DB.items():
+        if motif not in seq:
+            if motif[:10] in seq:
+                mutations.append(f"Partial mutation near {gene}")
+    return mutations
